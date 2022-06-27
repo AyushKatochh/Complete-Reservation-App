ecee8ca919791322eb9b280dc3ad8cca3cf7b574
Ayush Katoch <katochayushhp@gmail.com>
2022-06-27 22:11:14 +0530

    Update PropertyList.jsx

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,36 +1,60 @@
 import useFetch from "../../hooks/useFetch";
 import "./propertyList.css";
 
+const images = [
+  "https://ak-d.tripcdn.com/images/0222p1200082k9b8583D0_Z_1080_808_R5_D.jpg",
+  "https://ak-d.tripcdn.com/images/0224h1200082k9blh29A3_Z_1080_808_R5_D.jpg",
+  "https://d23ynomj6u3eig.cloudfront.net/sites/default/files/2020-03/67.%20gallery%20857x491-min.jpg",
+  "https://jkpages.com/wp-content/uploads/2021/05/eden-1024x768.jpg",
+  "https://www.outlookindia.com/outlooktraveller/public/uploads/2017/03/Raddison-Jammu.jpg"
+]
+
+
+
 const PropertyList = () => {
   const { data, loading, error } = useFetch("/hotels/countByType");
 
-  const images = [
-    "https://cf.bstatic.com/xdata/images/xphoto/square300/57584488.webp?k=bf724e4e9b9b75480bbe7fc675460a089ba6414fe4693b83ea3fdd8e938832a6&o=",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-apartments_300/9f60235dc09a3ac3f0a93adbc901c61ecd1ce72e.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/bg_resorts/6f87c6143fbd51a0bb5d15ca3b9cf84211ab0884.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-villas_300/dd0d7f8202676306a661aa4f0cf1ffab31286211.jpg",
-    "https://cf.bstatic.com/static/img/theme-index/carousel_320x240/card-image-chalet_300/8ee014fcc493cb3334e25893a1dee8c6d36ed0ba.jpg",
-  ];
+  
+
   return (
     <div className="pList">
       {loading ? (
         "loading"
       ) : (
         <>
-          {data &&
-            images.map((img,i) => (
-              <div className="pListItem" key={i}>
+         
+           
+              <div className="pListItem">
                 <img
+                  src={images[0]}
+                  alt=""
+                  className="pListImg"
+                />
+                  <img
                   src={images[1]}
                   alt=""
                   className="pListImg"
                 />
+                  <img
+                  src={images[2]}
+                  alt=""
+                  className="pListImg"
+                />
+                  <img
+                  src={images[3]}
+                  alt=""
+                  className="pListImg"
+                />
+                  <img
+                  src={images[4]}
+                  alt=""
+                  className="pListImg"
+                />
+                 
                 <div className="pListTitles">
-                  <h1>{data[i]?.type}</h1>
-                  <h2>{data[i]?.count} {data[i]?.type}</h2>
                 </div>
               </div>
-            ))}
+            
         </>
       )}
     </div>
